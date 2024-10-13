1ecbb1dfc7f2b170b2717851e7ddc651854e52c9
WilsonSong1 <G00418330@atu.ie>
2024-10-13 13:57:40 +0000

    Exercise 3(b)

--- 3.js
+++ 3.js
@@ -1,2 +1,19 @@
 //Array of strings 3(a)
 let tasks = ["Work", "Shopping", "Homework"];
+
+//3(b) i.
+const addTask = (task)=>{
+
+    //3(b) ii.
+    //Pushing a value to the array of tasks
+    tasks.push(task);
+
+    //3(b) iii.
+    console.log("New task added");
+
+    //3(b) iv.
+    console.log(tasks.length);
+}
+
+//calling function
+addTask("Sports");
