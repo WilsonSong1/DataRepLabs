dd1900dafaae232076784278ae1aa8feb16e40bc
WilsonSong1 <G00418330@atu.ie>
2024-10-13 13:55:34 +0000

    Exercise 2(c)

---
+++ 2c.js
@@ -0,0 +1,8 @@
+//Arrow function taking 2 arguements
+let z = (num1, num2) => {
+    //Returning the sum of 2 values
+    return num1 + num2;
+}
+
+//Calling function displaying the sum of 2 values
+console.log(z(2, 3));
